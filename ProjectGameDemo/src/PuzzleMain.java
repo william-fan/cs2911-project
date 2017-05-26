aafa2c1428df21eb91e8699d9a8f8e52849f0c4b
kick13 <kick13@users.noreply.github.com>
2017-05-26 12:38:12 +1000

    added documentation

--- ProjectGameDemo/src/PuzzleMain.java
+++ ProjectGameDemo/src/PuzzleMain.java
@@ -2,6 +2,14 @@ import javafx.application.Application;
 
 public class PuzzleMain {
 
+/*
+ * This is the class that holds the Main function. It opens the PuzzleHome
+ * class.
+ * 
+ * @authors 
+ * William Fan, Bob Guo, Charles Lu, Alexander Ong, Allan Wu
+ */	
+
 	public static void main(String[] args) {
 		Application.launch(PuzzleHome.class, args);
 	}
